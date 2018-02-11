05cb3741dca29648462967074b08a6ea3d8c9093
wurst-case <dylan.modell.promo@gmail.com>
2018-02-10 19:45:53 -0800

    fixing buildContract gitignore

--- src/ethereum/deploy.js
+++ src/ethereum/deploy.js
@@ -1,27 +1,27 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const compiledLoveLockChain = require('./build/LoveLockChain.json');
+const compiledLoveLockChain = require('./buildContract/LoveLockChain.json');
 
 const provider = new HDWalletProvider(
-	'era enter strike job legal book secret barrel waste error laundry live',
-	'https://rinkeby.infura.io/nFGVPlVOdrfz9ZYEKAc1'
+  'era enter strike job legal book secret barrel waste error laundry live',
+  'https://rinkeby.infura.io/nFGVPlVOdrfz9ZYEKAc1',
 );
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-	const accounts = await web3.eth.getAccounts();
+  const accounts = await web3.eth.getAccounts();
 
-	console.log('Attempting to deploy from account', accounts[0]);
+  console.log('Attempting to deploy from account', accounts[0]);
 
-	const result = await new web3.eth.Contract(
-		JSON.parse(compiledLoveLockChain.interface)
-	)
-		.deploy({
-			data: compiledLoveLockChain.bytecode
-		})
-		.send({ gas: '1000000', from: accounts[0] });
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledLoveLockChain.interface),
+  )
+    .deploy({
+      data: compiledLoveLockChain.bytecode,
+    })
+    .send({ gas: '1000000', from: accounts[0] });
 
-	console.log('Contract deployed to', result.options.address);
+  console.log('Contract deployed to', result.options.address);
 };
 deploy();
 
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -3,17 +3,33 @@ import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.css';
 import Link from '../../components/Link';
+import cardFactory from '../../ethereum/cardFactory.js';
+import { Card, Button } from 'semantic-ui-react';
 
 class Home extends React.Component {
-  static propTypes = {
-    news: PropTypes.arrayOf(
-      PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        link: PropTypes.string.isRequired,
-        content: PropTypes.string,
-      }),
-    ).isRequired,
-  };
+  static async getInitialProps() {
+    const locks = await cardFactory.methods.getHashes().call();
+
+    return { locks };
+  }
+
+  renderLocks() {
+    const items = this.props.locks.map(address => {
+      // this.setState({ cardMessage: getMessage(address) });
+
+      return {
+        header: address,
+        description: (
+          <Link route={`/cards/${address}`}>
+            <a>View Card</a>
+          </Link>
+        ),
+        fluid: true,
+      };
+    });
+
+    return <Card.Group items={items} />;
+  }
 
   render() {
     return (
